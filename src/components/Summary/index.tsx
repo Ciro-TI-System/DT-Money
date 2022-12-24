eaf5df069b111ed9e125ca28efe0b19554ff7c69
Ciro Batista da Silva <78507494+Ciro-TI-System@users.noreply.github.com>
2022-12-24 12:57:03 -0300

    Summary 'hook'
    Creating a 'hook' to separate the calculation from the component Summary.

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,30 +1,10 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react';
-import { useContext } from 'react';
-import { TransactionsContext } from '../../contexts/TransactionsContext';
+import { useSummary } from '../../hooks/useSummary';
 import { priceFormatter } from '../../utils/formatter';
 import { SummaryCard, SummaryContainer } from './styles';
 
 export function Summary() {
-  const { transactions } = useContext(TransactionsContext);
-
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price;
-        acc.total += transaction.price;
-      } else {
-        acc.outcome += transaction.price;
-        acc.total -= transaction.price;
-      }
-
-      return acc;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  );
+  const summary = useSummary();
 
   return (
     <SummaryContainer>
@@ -33,6 +13,7 @@ export function Summary() {
           <span>Entradas</span>
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
+
         <strong>{priceFormatter.format(summary.income)}</strong>
       </SummaryCard>
 
@@ -41,6 +22,7 @@ export function Summary() {
           <span>Saídas</span>
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
+
         <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
 
@@ -49,8 +31,10 @@ export function Summary() {
           <span>Total</span>
           <CurrencyDollar size={32} color="#fff" />
         </header>
+
         <strong>{priceFormatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   );
 }
+
